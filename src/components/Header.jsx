21e295e5434753144a75f05ca017255be71bb5a1
rioribeirods <rioribeirods@gmail.com>
2022-09-07 13:58:46 -0300

    :tada: init

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h2>uma seleção de produtos</h2>
+            <h1>especial para você</h1>
+            <h3>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</h3>
+        </header>
+    )
+}
+
+export default Header
