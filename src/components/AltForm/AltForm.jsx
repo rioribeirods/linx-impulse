4b8ef755b8614a5bc97689a5a38f4c6f7f079228
rioribeirods <rioribeirods@gmail.com>
2022-09-09 15:45:33 -0300

    :sparkles: feat: form inputs validation with useForm (register, required)

--- src/components/AltForm/AltForm.jsx
+++ src/components/AltForm/AltForm.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 import P from '../P/P'
 import Button from '../Button/Button';
 import style from './AltForm.module.css'
+import { useForm } from "react-hook-form"
 
 const AltForm = () => {
+    const { register: register, handleSubmit: handleSubmit, formState: { errors: error } } = useForm();
+    const onSubmit = data => console.log(data);
+  
     return (
         <section className={style.AltForm}>
             <div className={style.selection}>
@@ -13,16 +17,21 @@ const AltForm = () => {
                 <span className={style.line} />
             </div>
             <P style={style.p1} content={"Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!"} />
-            <section className={style.forms}>
+            <form onSubmit={handleSubmit(onSubmit)} className={style.forms}>
                 <div className={style.forms1}>
                     <label>Nome do seu amigo:</label>
-                    <input type='text' name='nome' className={style.inputText} />
+                    <input {...register("nome", { required: true, minLength: 10 })} type='text' name='nome' className={style.inputText} />
+                    {error.nome && <span>Esse campo é obrigatório.</span>}
                 </div>
                 <div className={style.forms2}>
                     <label>Email:</label>
-                    <input type='email' name='email' className={style.inputText} />
+                    <input {...register("email2", { required: true,  pattern: {
+      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+      message: 'Enter a valid e-mail address',
+    } })} type='email' name='email' className={style.inputText} />
+    {error.email && <span>Esse campo é obrigatório.</span>}
                 </div>
-            </section>
+            </form>
             <div className={style.divBtn}>
                 <Button content={'Enviar agora'} style={style.btn} />
             </div>
--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import style from "./Forms.module.css"
 import Button from '../Button/Button';
 
 const Forms = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <form className={style.form}>
+        <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
             <label>Seu nome:</label>
-            <input type="text" name='nome' className={style.inputTexto} />
+            <input type="text" name='nome' {...register("nome", { required: true, minLength: 10 })} className={style.inputTexto} />
+            {errors.nome && <span>Esse campo é obrigatório.</span>}
             <label>Email:</label>
-            <input type="email" name='email' className={style.inputTexto} />
-            <label>CPF:</label>
-            <input type="text" name='cpf' className={style.inputTexto} />
+            <input type="email" name='email' className={style.inputTexto} {...register("email", {
+                required: true, pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: 'Enter a valid e-mail address',
+                }
+            })} />
+            {errors.email && <span>Esse campo é obrigatório.</span>}
+            <label {...register("cpf", {
+                required: true, pattern: {
+                    value: /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+                    message: 'CPF inválido ',
+                }
+            })}>CPF:</label>
+            {errors.cpf && <span>Esse campo é obrigatório.<br></br>
+                (digite no formato xxx.xxx.xxx-xx)</span>}
+            <input type="text" name='cpf' className={style.inputTexto} {...register("cpf", {
+                required: true, pattern: {
+                    value: /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+                    message: 'CPF inválido ',
+                }
+            })} />
             <div className={style.inputRadio}>
-            <input type="radio" name='genero' />
-            <label>Masculino</label>
-            <input type="radio" name='genero' />
-            <label>Feminino</label>
-            <input type="radio" name='genero' />
-            <label>Não-binário</label>
+                <input  {...register("genero", { required: true})} type="radio" name='genero'/>
+                <label>Masculino</label>
+                <input {...register("genero", { required: true})} type="radio" name='genero'/>
+                <label>Feminino</label>
+                <input {...register("genero", { required: true})} type="radio" name='genero'/>
+                <label>Não-binário</label>
+                {errors.genero && <span>Esse campo é obrigatório.</span>}
             </div>
             <Button content={"Enviar"} style={style.button} />
         </form>
